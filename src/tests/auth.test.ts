9c24a6f96d812b215663b9998262e4ea10dd731a
kskaman <kskaman@gmail.com>
2025-08-06 19:50:19 -0230

    added tests for auth

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,16 +1,19 @@
 import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth.js";
 
-const person = {
-  isActive: true,
-  age: 32,
-};
-
-describe("person", () => {
-  test("person is defined", () => {
-    expect(person).toBeDefined();
+describe("getAPIKey", () => {
+  test("returns null if authorization header is missing", () => {
+    const headers = {};
+    expect(getAPIKey(headers)).toBeNull();
   });
 
-  test("is active", () => {
-    expect(person.isActive).toBeTruthy();
+  test("returns null if authorization format is wrong", () => {
+    const headers = { authorization: "Bearer abc123" };
+    expect(getAPIKey(headers)).toBeNull();
+  });
+
+  test("returns the API key if header is correctly formatted", () => {
+    const headers = { authorization: "ApiKey abc123" };
+    expect(getAPIKey(headers)).toBe("abc123"); // THIS WILL FAIL because return is commented
   });
 });
